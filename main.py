51a9bd40f2fb80e7fc4dca8972553000c5c02f3b
marcosturossi <54504387+marcosturossi@users.noreply.github.com>
2022-06-23 18:24:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app.router.include_router(routes.router)
 
 register_tortoise(
     app,
-    db_url="postgres://postgres:hd2biwnm@127.0.0.1:5431/tournament",
+    db_url="postgres://postgres:xxxxxxxxx@127.0.0.1:5431/tournament",
     modules={"models": ["src.models"]},
     generate_schemas=True,
     add_exception_handlers=True,
